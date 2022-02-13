65dad37550ba2aec0a4c24124848b26c7b09f717
Celia <celiamdona@gmail.com>
2022-02-13 14:50:47 -0500

    capture command-line arguments

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log('Hello Node!');
+const profileDataArgs = process.argv.slice(2, process.argv.lenght);
+
+const printProfileData = profileDataArr => {
+    profileDataArr.forEach(profileItem => console.log(profileItem));
+};
+
+printProfileData(profileDataArgs);
+
